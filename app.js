75ac4a1ecf533937bd67e79bad9086ca45e79e73
vinusham10 <vinusham05@gmail.com>
2024-01-19 11:42:40 +0530

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('react-container');
+ReactDOM.render("Hello! Welcome to React",container);
+
+// Functional component
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
+}
+// Render the Container
+ReactDOM.render(React.createElement(Container),container);
+
+class ReactContainer extends React.Component{
+    // constructor - to initialize the state 
+  
+    // render method to render the react dom 
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  ReactDOM.render(React.createElement(ReactContainer),container);
+
